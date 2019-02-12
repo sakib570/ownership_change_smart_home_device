1560bbbb940938519693c4b6820668d48385d873
Md Sakib Nizam Khan <sakib570@gmail.com>
2019-02-12 22:55:24 +0100

    feat: Add function to change Bluetooth device name

--- src/main/ownership_change.cpp
+++ src/main/ownership_change.cpp
@@ -1,5 +1,16 @@
 #include "ownership_change.h"
 
 int main(void){
+	change_device_name();
 	return 0;
+}
+
+void change_device_name(void){
+	int response;
+
+	response = system("hciconfig hci0 name 'OWCH_BT_Device'");
+	response = system("hciconfig hci0 down");
+	response = system("hciconfig hci0 up");
+	if(response == 0)
+		printf("Device Name Changed..\n");
 }
--- src/main/ownership_change.h
+++ src/main/ownership_change.h
@@ -1,4 +1,9 @@
 #ifndef OWNERSHIP_CHANGE_H_
 #define OWNERSHIP_CHANGE_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+
+void change_device_name(void);
+
 #endif /* OWNERSHIP_CHANGE_H_ */
