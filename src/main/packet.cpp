42f4905e4e33fc645e03ef4b1f9ac09352ecbeaf
msnkhan <msnkhan@kth.se>
2019-02-19 17:29:11 +0100

    feat: Add funtion for creating password confirmation packet

--- src/main/packet.cpp
+++ src/main/packet.cpp
@@ -16,3 +16,17 @@ struct generic_packet* create_ip_info_packet(){
 	return ip_info_packet;
 }
 
+struct generic_packet* create_pw_confirmation_packet(){
+	struct generic_packet *pw_confirmation_packet = (struct generic_packet*)malloc(sizeof(struct generic_packet));
+
+	pw_confirmation_packet->header.version = PROTOCOL_VERSION;
+	pw_confirmation_packet->header.message_type = MSG_PW_CONFIRMATION;
+	pw_confirmation_packet->header.reserved = RESERVED;
+	pw_confirmation_packet->header.payload_length = htons(sizeof(PW_CONFIRMATION_CODE));
+	memset(pw_confirmation_packet->header.sender_ip, '\0', 15);
+	strcpy(pw_confirmation_packet->header.sender_ip, get_own_ipadress());
+	sprintf(pw_confirmation_packet->header.sender_port,"%d", PORTNUM);
+	strcpy(pw_confirmation_packet->payload, PW_CONFIRMATION_CODE);
+
+	return pw_confirmation_packet;
+}
