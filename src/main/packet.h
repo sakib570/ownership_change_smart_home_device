aab280777abbd66bc741974b990b3269287ac5b5
msnkhan <msnkhan@kth.se>
2019-02-19 14:23:00 +0100

    feat: Define all protocol constants

--- src/main/packet.h
+++ src/main/packet.h
@@ -3,6 +3,10 @@
 
 #include <stdint.h>
 
+#define PORTNUM							 		6346
+#define PROTOCOL_VERSION		   				   9
+#define RESERVED					   		  0x0000
+
 #define MSG_IP_BROADCAST 				 		0x01
 #define MSG_IP_UNICAST					 		0x02
 #define MSG_PASSWORD					 		0x03
@@ -17,6 +21,15 @@
 #define MSG_CREATE_NEW_PROFILE_REQUEST          0x15
 #define MSG_PROFILE_CHOICE_RESPONSE      		0x18
 #define MSG_PROFILE_AUTHENTICATION_RESPONSE		0x19
+
+#define PW_CONFIRMATION_CODE           		"0xF9F9"
+#define CHALLENGE_REQUEST_CODE	  	   		"0x1391"
+#define CHALLENGE_RESPONSE_CODE	  	   		"0xE7B7"
+#define AUTHENTICATION_SUCCESS           	  "0x01"
+#define AUTHENTICATION_FAIL              	  "0x02"
+
+#define DEVICE_NAME			  	   "OWCH_IoT_DEVICE"
+
 
 
 struct PACKET_HEADER{
