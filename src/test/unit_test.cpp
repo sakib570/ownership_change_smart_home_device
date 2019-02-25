4fdaefdfd086a5fb03e44617669f19a18a45702b
msnkhan <msnkhan@kth.se>
2019-02-25 14:19:05 +0100

    test: Add test case to verify the correctness of create_pw_confirmation_packet function

--- src/test/unit_test.cpp
+++ src/test/unit_test.cpp
@@ -4,10 +4,11 @@ int main(){
 
 	is_test = true;
 	test_correctness_of_ip_info_packet_creation();
+	test_correctness_of_pw_confirmation_packet_creation();
 	return 0;
 }
 
-void test_correctness_of_ip_info_packet_creation(){
+void test_correctness_of_ip_info_packet_creation(void){
 
 	struct generic_packet* ip_info_packet = (struct generic_packet*)malloc(sizeof(struct generic_packet));
 	char* test_ip = (char*)"192.168.0.1";
@@ -37,6 +38,41 @@ void test_correctness_of_ip_info_packet_creation(){
 		printf("\n");
 		for(i=0;i<packet_length;i++)
 				printf("%d ",ip_info_packet_to_test[i]);
+		printf("\n");
+	}
+
+}
+
+void test_correctness_of_pw_confirmation_packet_creation(void){
+
+	struct generic_packet* pw_confirmation_packet = (struct generic_packet*)malloc(sizeof(struct generic_packet));
+	char* test_ip = (char*)"192.168.0.1";
+
+	pw_confirmation_packet->header.version = 9;
+	pw_confirmation_packet->header.message_type = 0x04;
+	pw_confirmation_packet->header.reserved = 0x0000;
+	pw_confirmation_packet->header.payload_length = htons(sizeof("0xF9F9"));
+	memset(pw_confirmation_packet->header.sender_ip, '\0', 15);
+	strcpy(pw_confirmation_packet->header.sender_ip, test_ip);
+	sprintf(pw_confirmation_packet->header.sender_port,"%d", 6346);
+	strcpy(pw_confirmation_packet->payload, "0xF9F9");
+
+	char *correct_pw_confirmation_packet = (char*) pw_confirmation_packet;
+
+	char* pw_confirmation_packet_to_test = (char *)create_pw_confirmation_packet();
+	int packet_length = (int)sizeof(PACKET_HEADER) + (int)sizeof(PW_CONFIRMATION_CODE);
+	int compare_value = memcmp(correct_pw_confirmation_packet, pw_confirmation_packet_to_test, packet_length);
+	if(compare_value == 0)
+		printf("Test passed\n");
+	else
+		printf("Test Failed\n");
+	if(DEBUG_LEVEL > 3){
+		int i;
+		for(i=0;i<packet_length;i++)
+			printf("%d ",correct_pw_confirmation_packet[i]);
+		printf("\n");
+		for(i=0;i<packet_length;i++)
+				printf("%d ",pw_confirmation_packet_to_test[i]);
 		printf("\n");
 	}
 
